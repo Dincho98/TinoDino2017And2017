a30764998872baa64350b18f2bc5c5e3342bf85d
valentinofus <fus.valentino@gmail.com>
2019-07-16 08:06:51 +0200

    dino reci mi ako dela

---
+++ Code-iT Trgovine -7519/PC POS/DelaJosJenput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCPOS
+{
+    public partial class DelaJosJenput : Form
+    {
+        public DelaJosJenput()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("jebote dela");
+        }
+    }
+}
